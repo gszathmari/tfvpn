135c055b045aceccf1e1afa123deb92bd31df5ed
Gabor Szathmari <gszathmari@gmail.com>
2021-08-21 01:52:04 +1000

    Update Terraform and Linode provider

--- providers.tf
+++ providers.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = "~> 0.15.3"
+  required_version = "~> 1.0.0"
   required_providers {
     linode = {
       source  = "linode/linode"
-      version = "~> 1.18.0"
+      version = "~> 1.20.1"
     }
     random = {
       source  = "hashicorp/random"
